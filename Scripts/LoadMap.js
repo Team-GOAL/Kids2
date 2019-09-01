11d7396ff974dfee3c82b7d5903d7bb87cab3b30
Gabe <gnai0001@student.monash.edu>
2019-09-01 20:46:29 +1000

    Add existing file

--- Scripts/LoadMap.js
+++
@@ -1,6 +0,0 @@
-<script>
-L.mapbox.accessToken = "pk.eyJ1Ijoiem9lMDkyNSIsImEiOiJjam5mNDV1bmgwM3NtM3ZxaXdmNmt2ZWk2In0.nF5xM1nKywEsPTmIf91tRw​";
-var map = L.mapbox.map('map')
-    .setView([-37.818078, 144.96681], 13)
-    .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/light-v10'));
-</script>
